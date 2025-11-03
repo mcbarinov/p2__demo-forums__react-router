5616451e3d27b6996b9d2ca45da7a915972ccb09
mcbarinov <mcbarinov@proton.me>
2025-11-03 18:08:49 +0300

    update

--- eslint.config.js
+++ eslint.config.js
@@ -12,49 +12,52 @@ import { defineConfig, globalIgnores } from "eslint/config"
 
 export default defineConfig([
   globalIgnores(["dist", "public/mockServiceWorker.js"]),
-  {
-    files: ["**/*.{ts,tsx}"],
-    extends: [
-      js.configs.recommended,
-      ...tseslint.configs.strictTypeChecked,
-      ...tseslint.configs.stylisticTypeChecked,
-      ...pluginQuery.configs["flat/recommended"],
-      reactX.configs["recommended-typescript"],
-      reactDom.configs.recommended,
-      reactHooks.configs["recommended-latest"],
-      reactRefresh.configs.vite,
-      prettier,
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-      parserOptions: {
-        project: ["./tsconfig.node.json", "./tsconfig.app.json"],
-        tsconfigRootDir: import.meta.dirname,
+  js.configs.recommended,
+  reactX.configs["recommended-typescript"],
+  reactDom.configs.recommended,
+  reactHooks.configs.flat["recommended-latest"],
+  reactRefresh.configs.vite,
+  prettier,
+  ...tseslint.config(
+    {
+      files: ["**/*.{ts,tsx}"],
+      extends: [
+        ...tseslint.configs.strictTypeChecked,
+        ...tseslint.configs.stylisticTypeChecked,
+        ...pluginQuery.configs["flat/recommended"],
+      ],
+      languageOptions: {
+        ecmaVersion: 2020,
+        globals: globals.browser,
+        parserOptions: {
+          project: ["./tsconfig.node.json", "./tsconfig.app.json"],
+          tsconfigRootDir: import.meta.dirname,
+        },
+      },
+      rules: {
+        "@typescript-eslint/no-misused-promises": [
+          "error",
+          {
+            checksVoidReturn: {
+              attributes: false,
+            },
+          },
+        ],
+        "@typescript-eslint/unified-signatures": "off",
       },
     },
-    rules: {
-      "@typescript-eslint/no-misused-promises": [
-        "error",
-        {
-          checksVoidReturn: {
-            attributes: false,
-          },
-        },
-      ],
-    },
-  },
-  {
-    files: ["**/components/ui/**/*.{ts,tsx}"],
-    rules: {
-      "@typescript-eslint/consistent-type-definitions": "off",
-      "@typescript-eslint/no-unnecessary-condition": "off",
-      "@typescript-eslint/no-unnecessary-template-expression": "off",
-      "@typescript-eslint/no-unnecessary-type-conversion": "off",
-      "react-refresh/only-export-components": "off",
-      "react-x/no-context-provider": "off",
-      "react-x/no-use-context": "off",
-      "react-x/no-unstable-context-value": "off",
-    },
-  },
+    {
+      files: ["**/components/ui/**/*.{ts,tsx}"],
+      rules: {
+        "@typescript-eslint/consistent-type-definitions": "off",
+        "@typescript-eslint/no-unnecessary-condition": "off",
+        "@typescript-eslint/no-unnecessary-template-expression": "off",
+        "@typescript-eslint/no-unnecessary-type-conversion": "off",
+        "react-refresh/only-export-components": "off",
+        "react-x/no-context-provider": "off",
+        "react-x/no-use-context": "off",
+        "react-x/no-unstable-context-value": "off",
+      },
+    }
+  ),
 ])
